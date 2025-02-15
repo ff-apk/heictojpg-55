ea72b470a8c32735825f71fbb6f28b7c9a7c1345
build-gg <94852783+build-gg@users.noreply.github.com>
2025-02-15 08:52:41 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,10 @@ const Header = () => {
   return (
     <div className="text-center">
       <h1 className="text-3xl font-bold text-foreground sm:text-4xl">
-        Clipboard to Image
+        HEIC to JPG
       </h1>
       <p className="mt-2 text-muted-foreground">
-        Paste Image to Download
+        Open, View and Convert your HEIC images at bulk
       </p>
     </div>
   );
