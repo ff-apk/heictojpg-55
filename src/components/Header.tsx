59f777c397405384499da0040eb1ec981b0e5018
build-gg <94852783+build-gg@users.noreply.github.com>
2025-02-15 08:51:54 +0530

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold text-foreground sm:text-4xl">
+        Clipboard to Image
+      </h1>
+      <p className="mt-2 text-muted-foreground">
+        Paste Image to Download
+      </p>
+    </div>
+  );
+};
+
+export default Header;
