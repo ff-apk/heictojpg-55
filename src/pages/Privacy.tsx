9a73faba6f6bab0a29780ef6bacaa4ad7fd1fb1f
Gen Gamer <privatehirakbhai35@gmail.com>
2025-02-20 13:33:49 +0530

    Update Privacy.tsx

--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -19,7 +19,8 @@ const Privacy = () => {
             <h1>Privacy Policy</h1>
 <p>Last updated: February 20, 2025</p>
 <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
-<p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p>
+<p>This Privacy Policy is applicable to domain (heictojpgpro.com) and apps (HEIC to JPG, HEIC Image Viewer) by developer/publisher Microtrend Media, Co</p>
+            <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p>
 <h2>Interpretation and Definitions</h2>
 <h3>Interpretation</h3>
 <p>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
